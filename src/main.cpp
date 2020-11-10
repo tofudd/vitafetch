5af26a5b1e723e83a7bde52ae3b5b9aa39fdf259
tofudd <bartlomiej.tota@outlook.com>
2020-11-10 18:07:53 +0100

    Battery info

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,44 @@
 #include <psp2/kernel/threadmgr.h>
 #include <psp2/kernel/processmgr.h>
+#include <psp2/power.h> 
+#include <psp2/kernel/sysmem.h> 
 #include <stdio.h>
-
 #include "debugScreen.h"
 
 
 #define printf psvDebugScreenPrintf
 
 int main(int argc, char *argv[]) {
+	
 	PsvDebugScreenFont *psvDebugScreenFont_default_1x;
 	PsvDebugScreenFont *psvDebugScreenFont_default_2x;
 	PsvDebugScreenFont *psvDebugScreenFont_previous;
 	PsvDebugScreenFont *psvDebugScreenFont_current;
-
+		
+	int batteryPercentage;
+	int batteryTemp;
+	int batteryCycles;
+	int batteryHealth;
+		
+	// Debug screen init
 	psvDebugScreenInit();
 	psvDebugScreenFont_previous = psvDebugScreenFont_default_1x = psvDebugScreenGetFont();
 	psvDebugScreenFont_default_2x = psvDebugScreenScaleFont2x(psvDebugScreenFont_default_1x);
-	psvDebugScreenFont_current = psvDebugScreenSetFont(psvDebugScreenFont_default_2x);
+	psvDebugScreenFont_current = psvDebugScreenSetFont(psvDebugScreenFont_default_2x);	
+	
+	// Fetching battery info
+	batteryPercentage = scePowerGetBatteryLifePercent();
+	batteryTemp = scePowerGetBatteryTemp();
+	batteryCycles = scePowerGetBatteryCycleCount();
+	batteryHealth = scePowerGetBatterySOH();
+	
+	printf("Battery level:\t %d%% \n", batteryPercentage);
+	printf("Battery temp:\t %dC \n", batteryTemp / 100);
+	printf("Battery cycles:\t %d \n", batteryCycles);
+	printf("Battery health:\t %d%% \n", batteryHealth);
+	
 
-	printf("VitaFetch");
-	sceKernelDelayThread(3*1000000);
-	sceKernelExitProcess(0);
-	return 0;
+
+	
+	return sceKernelDelayThread(~0);
 }
